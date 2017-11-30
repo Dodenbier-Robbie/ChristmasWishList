730baec3793591f1bc9f1cbf3abb060f25037564
Robbie Dodenbier <rdodenbier@byui.edu>
2017-11-30 16:21:38 -0700

    Add files via upload

---
+++ AndroidLogin/src/LoginServlet/LoginServlet.java
@@ -0,0 +1,59 @@
+package LoginServlet;
+
+import LoginServlet.DBConnectionHandler;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.Enumeration;
+import org.json.simple.JSONObject;
+ 
+@WebServlet("/login_servlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        JSONObject json = new JSONObject();
+
+        Enumeration<String> paramNames = request.getParameterNames();
+        String params[] = new String[2];
+        int i = 0;
+        while (paramNames.hasMoreElements()) {
+            String paramName = (String) paramNames.nextElement();
+            String[] paramValues = request.getParameterValues(paramName);
+            params[i] = paramValues[0];
+            i++;
+        }
+ 
+        String sql = "SELECT username, password FROM users where username=? and password=?";
+        Connection con = DBConnectionHandler.getConnection();
+         
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, params[0]);
+            ps.setString(2, params[1]);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                json.put("info","success");
+            } else {
+                json.put("info", "fail");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json.toString());
+    }
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
